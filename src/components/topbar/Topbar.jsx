060360533f0dbd65a23f2e4049283cf3a1cfa88f
rabianuristanbullu <rabianuristanbullu@icloud.com>
2023-09-11 17:46:39 +0300

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,32 @@
+import "./topbar.scss";
+// import {Person, Mail} from "@material-ui/icons";
+
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            istanbullu.
+          </a>
+          <div className="itemContainer">
+            {/* <Person className="icon" /> */}
+            <span>+90 541 431 26 99</span>
+          </div>
+          <div className="itemContainer">
+            {/* <Mail className="icon" /> */}
+            <span>rabianuristanbullu@icloud.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
